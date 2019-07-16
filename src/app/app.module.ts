155c7b010a217d86351952c4881ca612c8a6e011
2imad <imadyoussoufi@gmail.com>
2019-07-16 19:33:50 +0200

    add router and src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { BugService } from './shared/bug.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BugService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
